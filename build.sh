39e9a98971376dcf79c897756f2d2c7213f120a8
rsundahl <roy@polyverse.com>
2020-01-17 10:12:27 -0800

    Bump to ubuntu:xenial.

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ PV_EXP_NAME="exp-nginx-1.4.0-brop"
 main() {
         GIT_COMMIT=$(git rev-parse --verify HEAD)
 
-	docker build --build-arg PV_FROM=i386/ubuntu:trusty -t "$PV_EXP_NAME-32" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-32:latest" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-32:$GIT_COMMIT" .
+	docker build --build-arg PV_FROM=i386/ubuntu:xenial -t "$PV_EXP_NAME-32" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-32:latest" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-32:$GIT_COMMIT" .
         [ $? -ne 0 ] && return 1
 
-	docker build --build-arg PV_FROM=ubuntu:trusty -t "$PV_EXP_NAME-64" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-64:latest" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-64:$GIT_COMMIT" .
+	docker build --build-arg PV_FROM=ubuntu:xenial -t "$PV_EXP_NAME-64" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-64:latest" -t "$PV_DOCKER_REGISTRY/$PV_EXP_NAME-64:$GIT_COMMIT" .
         [ $? -ne 0 ] && return 1
 
 	return 0
